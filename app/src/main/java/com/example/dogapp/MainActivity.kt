4af598d561ea8f8edd86310221eb2b0e68838112
marvincampo311 <128555565+marvincampo311@users.noreply.github.com>
2025-05-12 11:49:22 -0500

    Initial commit

---
+++ app/src/main/java/com/example/dogapp/MainActivity.kt
@@ -0,0 +1,87 @@
+package com.example.dogapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
+import androidx.biometric.BiometricPrompt
+import androidx.core.content.ContextCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+        val biometricManager = androidx.biometric.BiometricManager.from(this)
+
+
+        when(biometricManager.canAuthenticate(BIOMETRIC_STRONG)){
+            androidx.biometric.BiometricManager.BIOMETRIC_SUCCESS -> {
+                showBiometricPrompt()
+
+            }
+
+            androidx.biometric.BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE ->{
+                showToast("Hardware biometrico no disponible")
+
+            }
+
+            androidx.biometric.BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE -> {
+                showToast("harware biometrico no ddisponible")
+
+            }
+
+            androidx.biometric.BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED -> {
+                showToast("no hay biometria registrada")
+
+            }
+
+        }
+
+
+    }
+
+    private fun showBiometricPrompt(){
+  val executor = ContextCompat.getMainExecutor(this)
+
+        val biometricPrompt = BiometricPrompt(this, executor,
+            object : BiometricPrompt.AuthenticationCallback(){
+                override fun onAuthenticationSucceeded(result: BiometricPrompt.AuthenticationResult) {
+                    super.onAuthenticationSucceeded(result)
+                    showToast("autenticacion exitosa")
+                }
+
+                override fun onAuthenticationError(errorCode: Int, errString: CharSequence) {
+                    super.onAuthenticationError(errorCode, errString)
+                    showToast("ERROR $errString")
+                }
+
+                override fun onAuthenticationFailed() {
+                    super.onAuthenticationFailed()
+                    showToast("autenticacion fallida")
+                }
+
+            })
+
+        val promptInfo = BiometricPrompt.PromptInfo.Builder()
+            .setTitle("AUTENTICAICON BIOMETRICA")
+            .setSubtitle("USA TU HUELLA DIJITAL PARA CONTINUAR")
+            .setNegativeButtonText("cCANCELAR")
+            .build()
+
+
+        biometricPrompt.authenticate(promptInfo)
+
+
+
+    }
+
+    private fun showToast(message: String){
+        Toast.makeText(this, message , Toast.LENGTH_LONG).show()
+        }
+    }
